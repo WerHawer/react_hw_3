e0e9918254cf3a1caba128c4c87a2ee69e33120e
WerHawer <werhawer@gmail.com>
2020-04-25 15:17:47 +0300

    Updates

---
+++ precache-manifest.0e33cde5673f22fecf8be86a89e24e4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aed7d0496d4a53e720ddbd1f1ecd6eb",
+    "url": "/react_hw_3/index.html"
+  },
+  {
+    "revision": "4ef4fc103d3fdf95d30d",
+    "url": "/react_hw_3/static/css/2.8c5c8dfd.chunk.css"
+  },
+  {
+    "revision": "c01e97d727f48a7e2829",
+    "url": "/react_hw_3/static/css/main.959c7994.chunk.css"
+  },
+  {
+    "revision": "4ef4fc103d3fdf95d30d",
+    "url": "/react_hw_3/static/js/2.51106f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hw_3/static/js/2.51106f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01e97d727f48a7e2829",
+    "url": "/react_hw_3/static/js/main.73519d26.chunk.js"
+  },
+  {
+    "revision": "27bbf5934cbfcb90e77d",
+    "url": "/react_hw_3/static/js/runtime-main.29029414.js"
+  }
+]);
